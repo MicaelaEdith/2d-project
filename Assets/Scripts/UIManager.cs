e521521ede5aea0861bdf2ac6c298af2c568c918
Micaela Edith <romeromicaelaedith@gmail.com>
2024-10-17 22:36:11 -0300

    pause menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,10 +10,29 @@ public class UIManager : MonoBehaviour
     private List<GameObject> gridItems;
     private List<GameObject> visibleItems = new List<GameObject>();
     public static UIManager instance;
+    [SerializeField]
+    private Image imgPause;
+    private bool pause = false;
+    [SerializeField]
+    private GameObject btnPause;
+    [SerializeField]
+    private GameObject menu;
+
 
     private void Awake()
     {
         instance = this;
+        imgPause.enabled = false;
+        menu.SetActive(false);
+    }
+
+    void Start()
+    {
+        foreach (GameObject item in gridItems)
+        {
+            item.SetActive(false);
+        }
+
     }
 
     public void ShowItem(int itemIndex)
@@ -38,11 +57,29 @@ public class UIManager : MonoBehaviour
         return visibleItems;
     }
 
-    private void Start()
+    public void OnButtonClick(string btnPause)
     {
-        foreach (GameObject item in gridItems)
-        {
-            item.SetActive(false);
-        }
+        PauseMenu(pause);
+        pause = !pause;
     }
+
+    public void PauseMenu(bool pause)
+    {
+        if (!pause)
+        {
+            imgPause.enabled = true;
+            Time.timeScale = 0f;
+            menu.SetActive(true);
+            btnPause.SetActive(false);
+        }
+        else
+        {
+            imgPause.enabled = false;
+            Time.timeScale = 1f;
+            menu.SetActive(false);
+            btnPause.SetActive(true);
+        }
+
+    }
+
 }
